309316d117d0091a4ee0dfd6df9f59ed67b24dcc
Renan Lacerda Lisboa <33421188+renanllisboa@users.noreply.github.com>
2019-06-30 00:09:20 -0300

    Update x3sql_to_csv.py

--- x3sql_to_csv.py
+++ x3sql_to_csv.py
@@ -3,7 +3,7 @@ import csv
 
 def x3sql_to_csv():
 
-    connection = pyodbc.connect("DRIVER={SQL Server}; server=SPON010113665; database=P12117MNTDB;uid=sa;pwd=totvs@1234")
+    connection = pyodbc.connect("DRIVER={SQL Server}; server=; database=;uid=sa;pwd=")
     cursor = connection.cursor()
     cursor.execute("select X3_CAMPO, X3_TIPO, X3_TAMANHO, X3_TITULO, X3_TITSPA, X3_TITENG from SX3T10")
     data = cursor.fetchall()
